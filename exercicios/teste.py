67bea8efed5632311d56a1c45c46e6981ff3c343
Victor Hugo Oliveira <74155297+Vickthor1@users.noreply.github.com>
2024-03-13 16:46:50 -0300

    Add files via upload

---
+++ exercicios/teste.py
@@ -0,0 +1,20 @@
+numero = int(input('Digite um inteiro: '))
+
+if (numero%2) == 0:
+    print("Par")
+else:
+    print("Ímpar")
+    
+print('O primeiro numero é: ')
+print('Multiplicação')
+print('')
+print('O segundo numero é: ',num2,'Soma')
+print('Multiplicação')
+print('')
+print('O terceiro numero é: ',num3,'Soma')
+
+print('')
+print('O quarto numero é: ',num4,'Soma')
+
+print('')
+print('O quinto numero é: ',num5,'Soma')
